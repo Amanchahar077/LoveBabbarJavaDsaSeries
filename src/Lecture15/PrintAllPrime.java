dac19b0b8eb60b6ca3930e7e10447e2dbd825f21
Amanchahar077 <143725955+Amanchahar077@users.noreply.github.com>
2026-04-16 22:06:20 +0530

    lecture15

--- src/Lecture15/PrintAllPrime.java
+++ src/Lecture15/PrintAllPrime.java
@@ -20,7 +20,7 @@ public class PrintAllPrime {
         if(n==2){
             return true;
         }
-        for(int i=2;i<n;i++){
+        for(int i=2;i*i<=n;i++){
             if(n%i==0){
                 return false;
             }
--- src/Lecture15/checkPrime.java
+++ src/Lecture15/checkPrime.java
@@ -4,7 +4,7 @@ public class checkPrime {
     public static void main(String[] args){
         int num = 8;
         boolean isPrime = true;
-        for(int i=2;i<num;i++){
+        for(int i=2;i*i<=num;i++){
             if(num%i==0){
                 isPrime=false;
                 break;
